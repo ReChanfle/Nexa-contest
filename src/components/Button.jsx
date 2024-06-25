43ded1db5c6ce1131c9038c249516b22907f4ccd
Sebastian <sebastian@galponderopa.com>
2024-06-25 00:06:28 -0300

    progres on logic for interactive play

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,47 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import '../../src/css/button.css';
 import {useGetRichListsQuery} from "../redux/features/getRichListQuery";
-import Table from "./Table";
 
-const Button = () => {
 
-    const [binaryString, setBinaryString] = useState('');
+const Button = ({text, onClick, id}) => {
+
     const [fetchTrigger, setFetchTrigger] = useState(false);
 
     const { data, error, isLoading, isSuccess, refetch } = useGetRichListsQuery(fetchTrigger);
 
-
     const handleFetchData = () => {
         setFetchTrigger(true);
-        console.log(data);
     };
 
-
-    const generateBinaryString = (length) => {
-        let binary = '';
-        for (let i = 0; i < length; i++) {
-            binary += Math.random() < 0.5 ? '0' : '1'; // Genera aleatoriamente 0 o 1
-        }
-        return binary;
-    };
-
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const newBinaryString = generateBinaryString(10);
-            setBinaryString(newBinaryString);
-        }, 50);
-
-        return () => clearInterval(interval);
-    }, []);
 
 
     return (
         <div>
-            <button className="gradient-button" onClick={handleFetchData}>
-                {binaryString}
+            <button className="gradient-button" onClick={onClick}  id={id} >
+                {text}
             </button>
-            <Table data={data} isLoading={isLoading} error={error} isSuccess={isSuccess}/>
+
         </div>
 
     );
