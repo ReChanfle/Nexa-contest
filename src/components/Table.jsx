606d8f9e84fbc3f7af08a0249d513ac231f20027
Sebastian <sebastian@galponderopa.com>
2024-06-26 20:18:58 -0300

    better visuals, responsive fonts, changed size of question, reduced size of font in footer, updated rtk apiquery, now its working with live data of the api of Nexa.

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import '../../src/css/table.css';
-const Table = ({ data }) => {
-
+import Spinner from "./Spinner";
+const Table = ({ data, isLoading, error }) => {
     return (
         <div className="table-container">
-            <table className="table">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Ticker</th>
-                    <th>Transaction Count</th>
-                </tr>
-                </thead>
-                <tbody>
-                { data && (
-                    data.map((item, index) => (
+            {isLoading ? (
+                <Spinner />
+            ) : error ? (
+                <div>Error: {error.message}</div>
+            ) : (
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Ticker</th>
+                        <th>Transaction Count</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data && data.map((item, index) => (
                         <tr key={index}>
                             <td>{item.name}</td>
                             <td>{item.ticker}</td>
                             <td>{item.txCount}</td>
                         </tr>
-                    ))
-                ) }
-                </tbody>
-            </table>
+                    ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
 
 export default Table;
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
-import {useDispatch, useSelector} from "react-redux";
-import { configureStore } from '@reduxjs/toolkit';
-import {persistStore} from "redux-persist";
-import {persistedRichListApiReducer, richListApiMiddleware} from "./features/getRichListQuery";
-import {persistedTotalTxCountApiReducer, totalTxCountApiMiddleware} from "./features/getTotalTxCountQuery";
-import {middleware,persistedApiReducer} from "./features/api";
+import { configureStore} from '@reduxjs/toolkit';
+import { persistedRichListApiReducer, richListApiMiddleware } from './features/getRichListQuery';
+import { persistStore } from 'redux-persist';
+
+
 
 export const store = configureStore({
     reducer: {
@@ -14,14 +13,7 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
-        }).concat(
-            richListApiMiddleware,
-           // totalTxCountApiMiddleware,
-            //middleware
-        ),
+        }).concat(richListApiMiddleware),
 });
 
 export const persistor = persistStore(store);
-
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = (selector) => useSelector(selector);
